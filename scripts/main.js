ab151266d10fc61453f906299eaac0b8a7175d6d
jkerfs <jkerfs@calpoly.edu>
2016-03-02 22:35:51 -0800

    allow users to upload json files

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,38 @@ $('.masthead-nav li a').click(function() {
   $(this).parent().addClass('active');
 });
 
-//Load json data if there is a content param
-var content = window.location.href.split('?content=');
-if(content.length == 2) {
-  $.getJSON( "examples/" + content[1], function( data ) {
-    $.each(data.concepts,function(i,d) {
-      $("#concepts").val(function(ind, val) {
-        return val + d + "\n";
-      });
+function renderData(data) {
+  $("#concepts").val("");
+  $("#roles").val("");
+  $.each(data.concepts, function(i, d) {
+    $("#concepts").val(function(ind, val) {
+      return val + d + "\n";
     });
-    $.each(data.roles, function(i,d) {
-      $("#roles").val(function(ind, val) {
-        return val + d + "\n";
-      });
+  });
+  $.each(data.roles, function(i, d) {
+    $("#roles").val(function(ind, val) {
+      return val + d + "\n";
     });
   });
 }
+//Load json data if there is a content param
+var content = window.location.href.split('?content=');
+if (content.length == 2) {
+  $.getJSON("examples/" + content[1], function(data) {
+    renderData(data);
+  });
+}
+
+//Allow user to upload json files
+function handleFileSelect(evt) {
+  var file = evt.target.files[0]; // FileList object
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = JSON.parse(e.target.result);
+    renderData(contents);
+    $(".upload-modal").modal('hide');
+  };
+  reader.readAsText(file);
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
