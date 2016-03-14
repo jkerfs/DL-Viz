d307a88d1e9e41a66a42dd5a1cef88495df37d89
skylar <blackhathedgehog@gmail.com>
2016-03-13 19:25:15 -0700

    initial impl of timePaging

--- scripts/main.js
+++ scripts/main.js
@@ -10,21 +10,27 @@ $('.modal').on('hidden.bs.modal', function () {
     $(".masthead-nav li:first-child").addClass('active');
 });
 
-function renderData(data) {
+function renderData(data, timePage) {
   $("#concepts").val("");
   $("#roles").val("");
   $("#tbox").val("");
-  $.each(data.concepts, function(i, d) {
+
+  if (!timePage)
+    timePage = 0;
+  $("#time-page").html('<h5>Time Page: ' + (timePage+1) + '/' + data.length + '</h5>');
+
+  var currentPage = data[timePage];
+  $.each(currentPage.concepts, function(i, d) {
     $("#concepts").val(function(ind, val) {
       return val + d + "\n";
     });
   });
-  $.each(data.roles, function(i, d) {
+  $.each(currentPage.roles, function(i, d) {
     $("#roles").val(function(ind, val) {
       return val + d + "\n";
     });
   });
-  $.each(data.tbox, function(i, d) {
+  $.each(currentPage.tbox, function(i, d) {
     $("#tbox").val(function(ind, val) {
       return val + d + "\n";
     });
@@ -36,6 +42,20 @@ var content = window.location.href.split('?content=');
 if (content.length == 2) {
   $.getJSON("examples/" + content[1], function(data) {
     renderData(data);
+    var currentPage = 0;
+    $('#time-page-left').on('click', function () {
+      currentPage -= 1;
+      if (currentPage < 0)
+        currentPage = 0;
+      renderData(data, currentPage);
+    });
+    $('#time-page-right').on('click', function () {
+      currentPage += 1;
+      if (currentPage >= data.length) {
+        currentPage = data.length;
+      }
+      renderData(data, currentPage);
+    });
     $('#refresh').click();
   });
 }
