06be15862198ba1adb6e6af11103135cf825e493
skylar <blackhathedgehog@gmail.com>
2016-03-13 18:21:51 -0700

    improving impl of EQUIV

--- scripts/parser.js
+++ scripts/parser.js
@@ -21,20 +21,17 @@ function parseRole(str) {
   var parts = str.match(re);
 
   parts[2].replace(" ", "");
-  var individuals = parts[2].split(",")
+  var individuals = parts[2].split(",");
   return {
     "role": parts[1],
     "individuals": individuals
   };
 }
 
-function parseRelation(str) {
-  if (str.indexOf("SUBSET") == -1)
-    return {};
-
-  var parts = str.split("SUBSET");
+function parseSubset(parts) {
   parts[0].replace(" ", "");
   parts[1].replace(" ", "");
+
   return {
     "left": parts[0].replace(" ", ""),
     "right": parts[1].replace(" ", "")
@@ -42,28 +39,21 @@ function parseRelation(str) {
 }
 
 // Assumes already checked that str contains equals.
-function parseEquals(str) {
-  var parts = str.split("EQUALS");
+function parseEquivalent(parts) {
   var right;
-  var operator;
+  var operator = null;
+  var operators = ["AND", "OR", "LTE", "LT", "GTE", "GT", "EQ"];
 
-  //Parses for AND
-  if (parts[1].indexOf("AND") > -1)
-  {
-    right = parts[1].split("AND");
-    operator = "AND";
+  for (var op in operators) {
+    if (operators.hasOwnProperty(op) && ~parts[1].indexOf(op)) {
+      operator = op;
+      right = parts[1].split(op);
+      break;
+    }
   }
-  //Parses for OR
-  else if (parts[1].indexOf("OR") > -1)
-  {
-    right = parts[1].split("OR");
-    operator = "OR";
-  }
-  //Currently set so if no OR and AND returns nothing
-  else
-  {
+
+  if (!operator)
     return {};
-  }
 
   right[0].trim();
   right[1].trim();
@@ -73,4 +63,13 @@ function parseEquals(str) {
     "right": right,
     "operator": operator
   };
+}
+
+function parseRelation(str) {
+  if (~str.indexOf("SUBSET"))
+    return parseSubset(str.split("SUBSET"));
+  //else if (~str.indexOf("EQUIV"))
+  //  return parseEquivalent(str.split("EQUIV"));
+  else
+    return {};
 }
